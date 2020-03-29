90cb16d47f9bb9a706ef41ddb3b30c5674f1256f
Tony Jefferson <tony@mail.rit.edu>
2020-03-29 11:42:23 -0400

    Step 17 working

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,7 +1,43 @@
+const models = require('../models');
+
+const { Domo } = models;
+
+console.log(`Domo = ${Domo}`);
+
 const makerPage = (req, res) => {
   res.render('app');
 };
 
+const makeDomo = (req, res) => {
+  if (!req.body.name || !req.body.age) {
+    return res.status(400).json({ error: 'Bah! Both name and age are required' });
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    owner: req.session.account._id,
+  };
+
+  const newDomo = new Domo.DomoModel(domoData);
+
+  const domoPromise = newDomo.save();
+
+  domoPromise.then(() => res.json({ redirect: '/maker' }));
+
+  domoPromise.catch((err) => {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Domo already exists' });
+    }
+
+    return res.status(400).json({ error: 'An error occurred' });
+  });
+
+  return domoPromise;
+};
+
 module.exports = {
   makerPage,
+  make: makeDomo,
 };
